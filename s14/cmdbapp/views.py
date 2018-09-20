cb1e00a0d6c3928222619c639411fa996694fdb1
xm369083227 <369083227@qq.com>
2018-09-20 12:05:44 +0800

    django 工程

--- s14/cmdbapp/views.py
+++ s14/cmdbapp/views.py
@@ -4,8 +4,8 @@ from django.shortcuts import render
 from django.shortcuts import HttpResponse
 from django.shortcuts import redirect
 
-def home(request):
-    return HttpResponse('<h1>HELLO Django</h1>')
+# def home(request):
+#     return HttpResponse('<h1>HELLO Django</h1>')
 
 # def login(request):
 #     #print(request.method)
@@ -29,13 +29,27 @@ def login(request):
     error_msg = ""
     if request.method == "POST":
         # 获取用户通过POST提交过来的数据
-        user = request.POST.get('user',None)
-        pwd = request.POST.get('pwd',None)
+        user = request.POST.get('user')
+        pwd = request.POST.get('pwd')
         if user == 'admin' and pwd == "123":
             # 去跳转到
             return redirect('http://www.baidu.com')
         else:
             # 用户密码不配
             error_msg = "用户名或密码错误"
-
+            #这里的‘error_msg’作为字典的key传到前台页面上的{{error_msg}}，与前台匹配
     return render(request,'login.html', {'error_msg': error_msg})
+
+def home(request):
+    user_list = [
+        {'username':'uzi','age':21,'gender':'男'},
+        {'username':'faker','age':22,'gender':'男'},
+        {'username':'mlxg','age':23,'gender':'男'},
+    ]
+    if request.method == "POST":
+        u = request.POST.get("username")
+        a = request.POST.get("age")
+        g = request.POST.get("gender")
+        tmp = {'username': u, 'age': a, 'gender': g}
+        user_list.append(tmp)
+    return render(request,'home.html',{'user_list':user_list})
